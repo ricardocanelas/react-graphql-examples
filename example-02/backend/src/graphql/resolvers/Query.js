d34809d86e8a750c1249b70af2f470c82415b440
Ricardo Canelas <hello@ricardocanelas.com>
2018-11-21 12:34:44 -0200

    [example-02] added pagination

--- example-02/backend/src/graphql/resolvers/Query.js
+++ example-02/backend/src/graphql/resolvers/Query.js
@@ -1,10 +1,34 @@
+import mongoose from 'mongoose'
 import Product from '../../models/Product'
 
 export default {
     product: async id => {
         return await Product.findOne(id)
     },
+
     products: async () => {
         return await Product.find({})
+            .sort({ _id: 1 })
+            .exec()
+    },
+
+    productsPagination: async (_, { cursor, limit = 100 }) => {
+        const cursorOptions = cursor ? { _id: { $gt: mongoose.Types.ObjectId(cursor) } } : {}
+        const results = await Product.find(cursorOptions)
+            .sort({ _id: 1 })
+            .limit(limit + 1)
+            .exec()
+
+        let hasNextPage = results.length > limit
+        let endCursor = results[results.length - 1].id
+
+        return {
+            totalCount: await Product.countDocuments({}),
+            edges: hasNextPage ? results.slice(0, -1) : results,
+            pageInfo: {
+                hasNextPage,
+                endCursor,
+            },
+        }
     },
 }
